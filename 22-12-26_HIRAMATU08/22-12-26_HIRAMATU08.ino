8095fe71f0cdeb434fa6dc3fd6870c7942da4b11
tokumikatu <t1okumikatu@gmail.com>
2022-12-26 18:06:18 +0900

    Add files via upload

---
+++ 22-12-26_HIRAMATU08/22-12-26_HIRAMATU08.ino
@@ -0,0 +1,19 @@
+#include <Nendo.h>
+Nendo nendo(4);
+void setup(){
+ nendo.setupA();//これは、削除しないでください
+}
+void loop(){
+  //--見本のLチカですので削除OKです
+  //nendo.dash();//50ms
+  //nendo.dash();
+  //nendo.dash();
+  nendo.dot();//2000ms
+  //nendo.dot();
+  //nendo.dot();
+  //-----------------
+digitalWrite(13,HIGH);
+delay(1000);
+digitalWrite(13,LOW);
+delay(1000);
+}           
---
+++ SETUP_HIRAMATU08/setupA.ino
@@ -0,0 +1,26 @@
+void setupA(){
+   Serial.begin(115200);
+    pinMode(13,OUTPUT);
+    WiFi.softAP(ssid,pass);
+    delay(100);
+    WiFi.softAPConfig(ip,ip,subnet);
+    IPAddress myIP = WiFi.softAPIP();   
+    server.begin();
+    Serial.print("SSID: ");
+    Serial.println(ssid);
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+    Serial.println("Server start!");
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.disconnect();
+    Serial.print("Wi-Fi Channel: ");
+    Serial.println(WiFi.channel());
+    server.begin();
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "I am HIRAMATU08 desu");
+  });
+    AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+    //server.begin();
+    Serial.println("HTTP server started");
+    AsyncElegantOTA.loop();
+}
