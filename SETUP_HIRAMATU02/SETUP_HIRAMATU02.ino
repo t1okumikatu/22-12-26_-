b520649f2677d845d4d833f47095f9ddbcbc5c5a
tokumikatu <t1okumikatu@gmail.com>
2022-12-26 16:58:08 +0900

    Add files via upload

---
+++ SETUP_HIRAMATU02/SETUP_HIRAMATU02.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <AsyncElegantOTA.h>
+const char ssid[] = "HIRAMATU02";
+const char pass[] = "";
+const IPAddress ip(192,168,4,2);
+const IPAddress subnet(255,255,255,0);
+AsyncWebServer server(80);
+void setup(){
+   setupA();
+}
+void loop(){
+digitalWrite(13,HIGH);
+delay(1000);
+digitalWrite(13,LOW);
+delay(1000);
+}           
