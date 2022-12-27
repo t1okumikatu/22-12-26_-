1db853c4f4e013874704f0e548c7910445d3f2a4
tokumikatu <t1okumikatu@gmail.com>
2022-12-27 10:31:03 +0900

    Add files via upload

---
+++ JROBO_10/22-12-27_JROBO_10/22-12-27_JROBO_10.ino
@@ -0,0 +1,19 @@
+#include <Nendo.h>
+Nendo nendo(13);
+void setup(){
+ nendo.setupA();//これは、削除しないでください
+}
+void loop(){
+  //--見本のLチカですので削除OKです
+  //nendo.dash();//50ms
+  //nendo.dash();
+  //nendo.dash();
+  //nendo.dot();//2000ms
+  //nendo.dot();
+  //nendo.dot();
+  //-----------------
+digitalWrite(13,HIGH);
+delay(300);
+digitalWrite(13,LOW);
+delay(1500);
+}           
